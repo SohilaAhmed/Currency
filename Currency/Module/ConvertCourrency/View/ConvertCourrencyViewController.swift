a10e4e6eb2c3adab183812c1f00ceff080c74f36
Sohila <sohilaahmed668@gmail.com>
2023-06-01 02:06:21 +0300

    add RxSwift Pod

--- Currency/Module/ConvertCourrency/View/ConvertCourrencyViewController.swift
+++ Currency/Module/ConvertCourrency/View/ConvertCourrencyViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConvertCourrencyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
