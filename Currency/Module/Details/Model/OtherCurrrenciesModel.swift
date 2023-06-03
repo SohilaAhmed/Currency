24eccc591bf3e200b850989a4ba50063cf32045f
Sohila <sohilaahmed668@gmail.com>
2023-06-04 02:25:05 +0300

    create Details folder in module

---
+++ Currency/Module/Details/Model/OtherCurrrenciesModel.swift
@@ -0,0 +1,10 @@
+//
+//  OtherCurrrenciesModel.swift
+//  Currency
+//
+//  Created by Sohila on 04/06/2023.
+//
+
+import Foundation
+
+ 
---
+++ Currency/Module/Details/View/DetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.swift
+//  Currency
+//
+//  Created by Sohila on 04/06/2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+}
